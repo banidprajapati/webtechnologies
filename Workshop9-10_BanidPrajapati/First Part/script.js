1d7de470f7d697b8f94b808b26d83309cce13286
Banid Prajapati <NP03CS4A220015@heraldcollege.edu.np>
2023-01-22 07:48:31 +0545

    week 9 and 10

---
+++ Workshop9-10_BanidPrajapati/First Part/script.js
@@ -0,0 +1,21 @@
+let country = 'Nepal';
+let continent = 'Asia';
+let population = '30.03 million';
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+
+language = 'Nepali';
+const country = 'Nepal';
+const continent = 'Asia';
+const isIsland = false;
+country = 'United Kingdom'
